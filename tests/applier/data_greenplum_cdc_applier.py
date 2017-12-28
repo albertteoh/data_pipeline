2d4bc6c828611d0e287bf7fd46f78e47729d4a5d
Albert Teoh <albert.teoh@gmail.com>
2017-12-28 17:20:12 +1100

    Remove sensitive data from applier tests. Add apache headers.

--- tests/applier/data_greenplum_cdc_applier.py
+++ tests/applier/data_greenplum_cdc_applier.py
@@ -59,31 +59,4 @@ tests = [
 
   )
 
-, TestCase(
-    description="Apply logminer redo statement containing escaped double-single quotes",
-    input_table_name="DE_SGI",
-    input_commit_statements=[
-        '',
-        'update "KE_AGENTDESKTOP"."DE_SGI" set "ID" = \'68283124\', "CUSTOMERNAME_" = \'DENZIL\', "CUSTOMERQUERY_" = \' \', "CUSTOMEREMAIL_" = \'denzilhooya@yahoo.com\', "CUSTOMERPRODUCT_" = \'SSC-application\', "CUSTOMERGENDER_" = \'Male\', "CUSTOMERQUOTENUMBER_" = \'null\', "REFERRALID_" = \'null\', "SURNAME_" = \'D\\\'\'ROZARIO\', "BRAND_" = \'sgio\', "URLLOCATION_" = \'/sgio/ssc/pbtm-arrears/home/step1\', "USERNAME_" = \'denzilhooya@yahoo.com\', "CHATSERVEREMAIL_" = \'Not Specified\', "CUSTOMERKANAPARTYID_" = \'3255123\', "USERAGENT_" = \'Mozilla/5.0 (Linux; Android 6.0.1; SM-G900I Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36\' where "ID" = \'68283124\' and "CUSTOMERNAME_" = \'DENZIL\' and "CUSTOMERQUERY_" = \' \' and "CUSTOMEREMAIL_" = \'denzilhooya@yahoo.com\' and "CUSTOMERPRODUCT_" = \'SSC-application\' and "CUSTOMERGENDER_" = \'Male\' and "CUSTOMERQUOTENUMBER_" = \'null\' and "REFERRALID_" = \'null\' and "SURNAME_" = \'D\\\'\'ROZARIO\' and "BRAND_" = \'sgio\' and "URLLOCATION_" = \'/sgio/ssc/pbtm-arrears/home/step1\' and "USERNAME_" = \'denzilhooya@yahoo.com\' and "CHATSERVEREMAIL_" = \'Not Specified\' and "CUSTOMERKANAPARTYID_" = \'3255123\' and "USERAGENT_" = \'Mozilla/5.0 (Linux; Android 6.0.1; SM-G900I Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36\'',
-        ''],
-    input_record_types=[const.START_OF_BATCH, const.DATA, const.END_OF_BATCH],
-    input_operation_codes=['', const.UPDATE, ''],
-    input_primary_key_fields="ID",
-    input_record_counts=[0, 0, 1],
-    input_commit_lsns=[0, 0, 0],
-    expect_sql_execute_called=[
-        None,
-        "UPDATE ctl.DE_SGI SET CUSTOMERGENDER_ = 'Male', CUSTOMERPRODUCT_ = 'SSC-application', CUSTOMERNAME_ = 'DENZIL', CHATSERVEREMAIL_ = 'Not Specified', CUSTOMERKANAPARTYID_ = '3255123', USERNAME_ = 'denzilhooya@yahoo.com', CUSTOMERQUERY_ = ' ', CUSTOMEREMAIL_ = 'denzilhooya@yahoo.com', REFERRALID_ = 'null', CUSTOMERQUOTENUMBER_ = 'null', URLLOCATION_ = '/sgio/ssc/pbtm-arrears/home/step1', BRAND_ = 'sgio', SURNAME_ = 'D\\''ROZARIO', USERAGENT_ = 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G900I Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36' WHERE ID = '68283124'; -- lsn: 0, offset: 1",
-        None],
-    expect_execute_called_times=[0, 1, 1],
-    expect_audit_db_execute_sql_called=[None, None, (UPDATE_SSP_SQL, ('CDCApply', 0, 'myprofile', 1, 'ctl', 'de_sgi'))],
-    expect_commit_called_times=[0, 0, 1],
-    expect_insert_row_count=[0, 0, 0],
-    expect_update_row_count=[0, 1, 1],
-    expect_delete_row_count=[0, 0, 0],
-    expect_source_row_count=[0, 1, 1],
-    expect_batch_committed=[const.UNCOMMITTED, const.UNCOMMITTED, const.COMMITTED]
-  )
-
-
 ]
--- tests/applier/data_postgres_cdc_applier_bulkapply.py
+++ tests/applier/data_postgres_cdc_applier_bulkapply.py
@@ -148,90 +148,6 @@ tests=[
   )
 
 , TestCase(
-    description="Apply logminer redo statement containing parentheses in values with end of batch",
-    input_table_name="IAG_MVDETAIL",
-    input_commit_statements=[
-        '',
-        """insert into "KE_AGENTDESKTOP"."IAG_MVDETAIL"("COMPANY","SEQUENCE","MVYEAR","MVMAKE","MVMODEL","MVSERIES","MVBODY","MVENGTYP","MVENGCAP","MVEQUIP","STDEQUIP","MVTARE") values ('1','124049','2010','LOTUS','EVORA',NULL,'COUPE','FI','35','(2 SEAT)','ABB ABS AC  AL  AW1 CLR EBD EDL IM  LSW PM  PS  PW  RCD RS  SPS TC  TCS','0')""",
-        ''],
-    input_record_types=[const.START_OF_BATCH, const.DATA, const.END_OF_BATCH],
-    input_operation_codes=['', const.INSERT, ''],
-    input_primary_key_fields="COMPANY",
-    input_record_counts=[0, 0, 1],
-    input_commit_lsns=[0, 0, 0],
-    expect_sql_execute_called=[
-        None,
-        None,
-        "INSERT INTO ctl.IAG_MVDETAIL ( COMPANY, MVBODY, MVENGCAP, MVENGTYP, MVEQUIP, MVMAKE, MVMODEL, MVSERIES, MVTARE, MVYEAR, SEQUENCE, STDEQUIP ) VALUES\n  ( '1', 'COUPE', '35', 'FI', '(2 SEAT)', 'LOTUS', 'EVORA', NULL, '0', '2010', '124049', 'ABB ABS AC  AL  AW1 CLR EBD EDL IM  LSW PM  PS  PW  RCD RS  SPS TC  TCS' ) -- lsn: 0, offset: 1\n; -- lsn: 0, offset: 1",
-    ],
-                                      
-    expect_execute_called_times=[0, 0, 1],
-    expect_audit_db_execute_sql_called=[None, None, (UPDATE_SSP_SQL, ('CDCApply', 0, 'myprofile', 1, 'ctl', 'iag_mvdetail'))],
-    expect_commit_called_times=[0, 0, 1],
-    expect_insert_row_count=[0, 0, 1],
-    expect_update_row_count=[0, 0, 0],
-    expect_delete_row_count=[0, 0, 0],
-    expect_source_row_count=[0, 1, 1],
-    expect_batch_committed=[const.UNCOMMITTED, const.UNCOMMITTED, const.COMMITTED]
-  )
-
-
-, TestCase(
-    description="Apply logminer redo statement containing percent char in values with end of batch",
-    input_table_name="IAG_MVDETAIL",
-    input_commit_statements=[
-        '',
-        """insert into "KE_AGENTDESKTOP"."IAG_MVDETAIL"("COMPANY","SEQUENCE","MVYEAR","MVMAKE","MVMODEL","MVSERIES","MVBODY","MVENGTYP","MVENGCAP","MVEQUIP","STDEQUIP","MVTARE") values ('1','124049','2010','LOTUS','EVORA',NULL,'COUPE','FI','35','20%','ABB ABS AC  AL  AW1 CLR EBD EDL IM  LSW PM  PS  PW  RCD RS  SPS TC  TCS','0'); -- lsn: 0, offset: 1""",
-        ''],
-    input_record_types=[const.START_OF_BATCH, const.DATA, const.END_OF_BATCH],
-    input_operation_codes=['', const.INSERT, ''],
-    input_primary_key_fields="COMPANY",
-    input_record_counts=[0, 0, 1],
-    input_commit_lsns=[0, 0, 0],
-    expect_sql_execute_called=[
-        None,
-        None,
-        "INSERT INTO ctl.IAG_MVDETAIL ( COMPANY, MVBODY, MVENGCAP, MVENGTYP, MVEQUIP, MVMAKE, MVMODEL, MVSERIES, MVTARE, MVYEAR, SEQUENCE, STDEQUIP ) VALUES\n  ( '1', 'COUPE', '35', 'FI', '20%%', 'LOTUS', 'EVORA', NULL, '0', '2010', '124049', 'ABB ABS AC  AL  AW1 CLR EBD EDL IM  LSW PM  PS  PW  RCD RS  SPS TC  TCS' ) -- lsn: 0, offset: 1\n; -- lsn: 0, offset: 1",
-    ],
-                                      
-    expect_execute_called_times=[0, 0, 1],
-    expect_audit_db_execute_sql_called=[None, None, (UPDATE_SSP_SQL, ('CDCApply', 0, 'myprofile', 1, 'ctl', 'iag_mvdetail'))],
-    expect_commit_called_times=[0, 0, 1],
-    expect_insert_row_count=[0, 0, 1],
-    expect_update_row_count=[0, 0, 0],
-    expect_delete_row_count=[0, 0, 0],
-    expect_source_row_count=[0, 1, 1],
-    expect_batch_committed=[const.UNCOMMITTED, const.UNCOMMITTED, const.COMMITTED]
-  )
-
-
-, TestCase(
-    description="Apply logminer redo statement containing escaped double-single quotes",
-    input_table_name="DE_SGI",
-    input_commit_statements=[
-        '',
-        'update "KE_AGENTDESKTOP"."DE_SGI" set "ID" = \'68283124\', "CUSTOMERNAME_" = \'DENZIL\', "CUSTOMERQUERY_" = \' \', "CUSTOMEREMAIL_" = \'denzilhooya@yahoo.com\', "CUSTOMERPRODUCT_" = \'SSC-application\', "CUSTOMERGENDER_" = \'Male\', "CUSTOMERQUOTENUMBER_" = \'null\', "REFERRALID_" = \'null\', "SURNAME_" = \'D\\\'\'ROZARIO\', "BRAND_" = \'sgio\', "URLLOCATION_" = \'/sgio/ssc/pbtm-arrears/home/step1\', "USERNAME_" = \'denzilhooya@yahoo.com\', "CHATSERVEREMAIL_" = \'Not Specified\', "CUSTOMERKANAPARTYID_" = \'3255123\', "USERAGENT_" = \'Mozilla/5.0 (Linux; Android 6.0.1; SM-G900I Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36\' where "ID" = \'68283124\' and "CUSTOMERNAME_" = \'DENZIL\' and "CUSTOMERQUERY_" = \' \' and "CUSTOMEREMAIL_" = \'denzilhooya@yahoo.com\' and "CUSTOMERPRODUCT_" = \'SSC-application\' and "CUSTOMERGENDER_" = \'Male\' and "CUSTOMERQUOTENUMBER_" = \'null\' and "REFERRALID_" = \'null\' and "SURNAME_" = \'D\\\'\'ROZARIO\' and "BRAND_" = \'sgio\' and "URLLOCATION_" = \'/sgio/ssc/pbtm-arrears/home/step1\' and "USERNAME_" = \'denzilhooya@yahoo.com\' and "CHATSERVEREMAIL_" = \'Not Specified\' and "CUSTOMERKANAPARTYID_" = \'3255123\' and "USERAGENT_" = \'Mozilla/5.0 (Linux; Android 6.0.1; SM-G900I Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36\'',
-        ''],
-    input_record_types=[const.START_OF_BATCH, const.DATA, const.END_OF_BATCH],
-    input_operation_codes=['', const.UPDATE, ''],
-    input_primary_key_fields="ID",
-    input_record_counts=[0, 0, 1],
-    input_commit_lsns=[0, 0, 0],
-    expect_sql_execute_called=[
-        None,
-        "UPDATE ctl.DE_SGI SET CUSTOMERGENDER_ = 'Male', CUSTOMERPRODUCT_ = 'SSC-application', CUSTOMERNAME_ = 'DENZIL', CHATSERVEREMAIL_ = 'Not Specified', CUSTOMERKANAPARTYID_ = '3255123', USERNAME_ = 'denzilhooya@yahoo.com', CUSTOMERQUERY_ = ' ', CUSTOMEREMAIL_ = 'denzilhooya@yahoo.com', REFERRALID_ = 'null', CUSTOMERQUOTENUMBER_ = 'null', URLLOCATION_ = '/sgio/ssc/pbtm-arrears/home/step1', BRAND_ = 'sgio', ID = '68283124', SURNAME_ = 'D\\''ROZARIO', USERAGENT_ = 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G900I Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36' WHERE ID = '68283124'; -- lsn: 0, offset: 1",
-        None],
-    expect_execute_called_times=[0, 1, 1],
-    expect_audit_db_execute_sql_called=[None, None, (UPDATE_SSP_SQL, ('CDCApply', 0, 'myprofile', 1, 'ctl', 'de_sgi'))],
-    expect_commit_called_times=[0, 0, 1],
-    expect_insert_row_count=[0, 0, 0],
-    expect_update_row_count=[0, 1, 1],
-    expect_delete_row_count=[0, 0, 0],
-    expect_source_row_count=[0, 1, 1],
-    expect_batch_committed=[const.UNCOMMITTED, const.UNCOMMITTED, const.COMMITTED]
-  )
-
-, TestCase(
     description="Apply logminer insert statement with null with end of batch",
     input_table_name="CONNCT_CDC_PK5_COLS10",
     input_commit_statements=[
@@ -734,16 +650,16 @@ tests=[
 
 , TestCase(
     description="Apply alter MODIFY statement",
-    input_table_name="CWQ_QUEUED_WORK_TYPE",
-    input_commit_statements=['', 'ALTER TABLE CWQ_QUEUED_WORK_TYPE MODIFY DESCRIPTION VARCHAR2(400 CHAR) ', ''],
+    input_table_name="MYTABLE",
+    input_commit_statements=['', 'ALTER TABLE MYTABLE MODIFY DESCRIPTION VARCHAR2(400 CHAR) ', ''],
     input_record_types=[const.START_OF_BATCH, const.DATA, const.END_OF_BATCH],
     input_operation_codes=['', const.DDL, ''],
     input_primary_key_fields=None,
     input_record_counts=[0, 0, 1],
     input_commit_lsns=[0, 0, 0],
-    expect_sql_execute_called=[None, "ALTER TABLE ctl.CWQ_QUEUED_WORK_TYPE ALTER DESCRIPTION TYPE VARCHAR(400); -- lsn: 0, offset: 1", None],
+    expect_sql_execute_called=[None, "ALTER TABLE ctl.MYTABLE ALTER DESCRIPTION TYPE VARCHAR(400); -- lsn: 0, offset: 1", None],
     expect_execute_called_times=[0, 1, 1],
-    expect_audit_db_execute_sql_called=[None, None, (UPDATE_SSP_SQL, ('CDCApply', 0, 'myprofile', 1, 'ctl', 'cwq_queued_work_type'))],
+    expect_audit_db_execute_sql_called=[None, None, (UPDATE_SSP_SQL, ('CDCApply', 0, 'myprofile', 1, 'ctl', 'mytable'))],
     expect_commit_called_times=[0, 0, 1],
     expect_insert_row_count=[0, 0, 0],
     expect_update_row_count=[0, 0, 0],
@@ -754,16 +670,16 @@ tests=[
 
 , TestCase(
     description="Apply alter ADD statement",
-    input_table_name="CWQ_QUEUED_WORK_TYPE",
-    input_commit_statements=['', 'ALTER TABLE CWQ_QUEUED_WORK_TYPE ADD DESCRIPTION VARCHAR2(400 CHAR) ', ''],
+    input_table_name="MYTABLE",
+    input_commit_statements=['', 'ALTER TABLE MYTABLE ADD DESCRIPTION VARCHAR2(400 CHAR) ', ''],
     input_record_types=[const.START_OF_BATCH, const.DATA, const.END_OF_BATCH],
     input_operation_codes=['', const.DDL, ''],
     input_primary_key_fields=None,
     input_record_counts=[0, 0, 1],
     input_commit_lsns=[0, 0, 0],
-    expect_sql_execute_called=[None, "ALTER TABLE ctl.CWQ_QUEUED_WORK_TYPE ADD DESCRIPTION VARCHAR(400); -- lsn: 0, offset: 1", None],
+    expect_sql_execute_called=[None, "ALTER TABLE ctl.MYTABLE ADD DESCRIPTION VARCHAR(400); -- lsn: 0, offset: 1", None],
     expect_execute_called_times=[0, 1, 1],
-    expect_audit_db_execute_sql_called=[None, None, (UPDATE_SSP_SQL, ('CDCApply', 0, 'myprofile', 1, 'ctl', 'cwq_queued_work_type'))],
+    expect_audit_db_execute_sql_called=[None, None, (UPDATE_SSP_SQL, ('CDCApply', 0, 'myprofile', 1, 'ctl', 'mytable'))],
     expect_commit_called_times=[0, 0, 1],
     expect_insert_row_count=[0, 0, 0],
     expect_update_row_count=[0, 0, 0],
